4b671c6e17b0497ab1999f6742944d20b7388d47
testGit <extraksw@naver.com>
2025-06-19 14:11:11 +0900

    dd

--- Koco/src/main/java/icet/koco/global/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package icet.koco.global.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- Koco/src/main/java/icet/koco/problemSet/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package icet.koco.problemSet.repository;
-
-import icet.koco.problemSet.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
